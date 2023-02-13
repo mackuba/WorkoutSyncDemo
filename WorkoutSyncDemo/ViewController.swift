b7f477aa54aa7fbe6fdd4833067e588f698a1251
Kuba Suder <jakub.suder@gmail.com>
2023-02-13 16:22:20 +0200

    created project from the template

---
+++ WorkoutSyncDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WorkoutSyncDemo
+//
+//  Created by Kuba Suder on 13/02/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
